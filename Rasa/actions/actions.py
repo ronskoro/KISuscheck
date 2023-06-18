ed13b9bebd85e8e199baf077a3d6bf118767f96f
MNakhla <maria.bassem3@gmail.com>
2023-06-18 21:13:11 +0200

    answer FAQs using external model

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -10,6 +10,109 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 import requests
 
+import numpy as np
+import sys
+# sys.path.append('C:/Users/maria/anaconda3/envs/rasa-faq/Lib/site-packages/torch/torch._C')
+sys.path.append('C:/Users/maria/anaconda3/envs/KI-SusCheck-faq/Lib/site-packages')
+import json
+import torch
+from sentence_transformers import SentenceTransformer
+
+# sentence embedding selection
+sentence_transformer_select=True
+pretrained_model='bert-base-nli-mean-tokens' # Refer: https://github.com/UKPLab/sentence-transformers/blob/master/docs/pretrained-models/nli-models.md
+score_threshold = 0.70  # This confidence scores can be adjusted based on your need!!
+
+# Custom Action
+
+class ActionGetFAQAnswer(Action):
+
+    def __init__(self):
+        print("ActionGetFAQAnswer init")
+        super(ActionGetFAQAnswer, self).__init__()
+        self.faq_data = json.load(open("./data/faq.json", "rt", encoding="utf-8"))
+        self.sentence_embedding_choose(sentence_transformer_select, pretrained_model)
+        self.standard_questions_encoder = np.load("./data/standard_questions.npy")
+        self.standard_questions_encoder_len = np.load("./data/standard_questions_len.npy")
+        print(self.standard_questions_encoder.shape)
+
+    def sentence_embedding_choose(self, sentence_transformer_select=True, pretrained_model='bert-base-nli-mean-tokens'):
+        self.sentence_transformer_select = sentence_transformer_select
+        if sentence_transformer_select:
+            self.bc = SentenceTransformer(pretrained_model)
+
+    def get_most_similar_standard_question_id(self, query_question):
+        if self.sentence_transformer_select:
+            query_vector = torch.tensor(self.bc.encode([query_question])[0]).numpy()
+        else:
+            query_vector = self.bc.encode([query_question])[0]
+        print("Question received at action engineer")
+        score = np.sum((self.standard_questions_encoder * query_vector), axis=1) / (
+                self.standard_questions_encoder_len * (np.sum(query_vector * query_vector) ** 0.5))
+        top_id = np.argsort(score)[::-1][0]
+        return top_id, score[top_id]
+
+    def name(self) -> Text:
+        return "action_faq_get_answer"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        query = tracker.latest_message['text']
+        # print(query)
+        most_similar_id, score = self.get_most_similar_standard_question_id(query)
+        print("The question is matched with id:{} with score: {}".format(most_similar_id,score))
+        if float(score) > score_threshold: # This confidence scores can be adjusted based on your need!!
+            response = self.faq_data[most_similar_id]['a']
+            dispatcher.utter_message(response)
+            if(self.faq_data[most_similar_id].get('img') is not None):
+                imgs = self.faq_data[most_similar_id]['img']
+                if(len(imgs)>0):
+                    for i in imgs:
+                        print(i)
+                        dispatcher.utter_message(image=i)
+            if(self.faq_data[most_similar_id].get('sources') is not None):
+                resources = self.faq_data[most_similar_id]['sources']
+                if(len(resources)>0):
+                    dispatcher.utter_message("You can find more information about this topic here: ")
+                    for i in resources:
+                        dispatcher.utter_message(i)
+                else:
+                    dispatcher.utter_message(resources)
+        
+            # dispatcher.utter_message("Problem solved?")
+        else:
+            response = "Sorry, this question is beyond my ability..."
+            print(response)
+            dispatcher.utter_message(response)
+            dispatcher.utter_message("Sorry, I can't answer your question. You can dial the manual service...")
+        return []
+
+
+def encode_standard_question(sentence_transformer_select=True, pretrained_model='bert-base-nli-mean-tokens'):
+    """
+    This will encode all the questions available in question database into sentence embedding. The result will be stored into numpy array for comparision purpose.
+    """
+    if sentence_transformer_select:
+        bc = SentenceTransformer(pretrained_model)
+    # else:
+    #     bc = BertClient(check_version=False)
+    data = json.load(open("./data/faq.json", "rt", encoding="utf-8"))
+    standard_questions = [each['q'] for each in data]
+    print("Standard question size", len(standard_questions))
+    print("Start to calculate encoder....")
+    if sentence_transformer_select:
+        standard_questions_encoder = torch.tensor(bc.encode(standard_questions)).numpy()
+    # else:
+    #     standard_questions_encoder = bc.encode(standard_questions)
+    np.save("./data/standard_questions", standard_questions_encoder)
+    standard_questions_encoder_len = np.sqrt(np.sum(standard_questions_encoder * standard_questions_encoder, axis=1))
+    np.save("./data/standard_questions_len", standard_questions_encoder_len)
+
+# encode_standard_question(sentence_transformer_select,pretrained_model)
+# x = ActionGetFAQAnswer()
+# x.run()
+
 class getProductInfoByBarcode(Action):
     def name(self) -> Text:
         return "action_get_product_info_by_barcode"
@@ -438,4 +541,3 @@ class ActionPrintPreferences(Action):
             dispatcher.utter_message(text="Your preferences are: " + msg)
         
         return []
-
